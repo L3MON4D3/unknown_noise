b8a1c12e89ab94300e1baf4e8b8d24239d96d271
L3MON4D3 <Simljk@outlook.de>
2020-10-28 13:40:13 +0100

    Change Noise Modification-function, actually seed rand.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <cstdlib>
 #include <functional>
 
@@ -5,24 +7,32 @@
 
 #include "UnknownNoise.hpp"
 
+const std::function<float(float x, float a)> witch {
+	[](float x, float a) {
+		return (8*std::pow(a, 3))/(std::pow(x, 2)+ 4*std::pow(a, 2));
+	}
+};
+
 const std::function<float(int x)> const_mod {
 	[](int x) {
-		return 5;
+		return witch(x*(50.0/600)-25, 2)*10;
 	}
 };
 
 const std::function<float(float noise, int x)> no_filter {
 	[](float noise, int x) {
-		return noise;
+		return noise - witch(x*(50.0/600)-25, 2)*10;
 	}
 };
 
 int main(int argc, char** argv) {
 	FastNoise fn;
-	fn.SetNoiseType(FastNoise::Perlin);
+	fn.SetNoiseType(FastNoise::Simplex);
+
+	srand(time(0));
 	fn.SetSeed(std::rand());
 
 	//res_stretch_sz should be equal to hor_count.
-	unknown_noise::make_picture({{0,0}, {1000, 1500}, 1, 100, 1000, 40},
-		fn, {5, 1000, const_mod, no_filter}, "unknown_noise.png");
+	unknown_noise::make_picture({{190, 225}, {680, 1200}, .5, 30, 600, 2},
+		fn, {1, 600, const_mod, no_filter}, "unknown_noise.png");
 }
